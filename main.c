0a80065a94b5d40b77a9a70334cd7279f5b261f8
Alain Pascal <ialainpascal@gmail.com>
2022-06-16 18:32:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include "monty.h"
 
-bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-*
-* Return: 0 on success
-*/
+ * main - monty code interpreter
+ * @argc: number of arguments
+ * @argv: monty file location
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
+	bus_t bus = {NULL, NULL, NULL, 0};
 	char *content;
 	FILE *file;
 	size_t size = 0;
@@ -44,6 +43,6 @@ int main(int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
-	
+
 	return (0);
 }
